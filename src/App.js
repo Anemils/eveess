32e88c51f6964a89462a14a86f9f74fe89dcb1ce
Emils <52511322+Anemils@users.noreply.github.com>
2019-07-06 16:44:57 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './Form';
+import logo from './logo.svg';
+import './App.sass';
+
+function App() {
+
+  return(
+    <div></div>
+  );
+}
+
+export default App;
